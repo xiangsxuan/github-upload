87e06b45ae6c43c065b38526167f99f45afa3d84
xiangshouxuan <xiangshouxuan@foxmail.com>
2019-10-12 10:34:20 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+			Console.WriteLine("Hello Git!");
+			Console.WriteLine("Hello C#!");
+        }
+		void Print(){
+			Console.WriteLine("Hello WPF");
+		}
+    }
+}
